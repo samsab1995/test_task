a913327080047e8673b281ac6327db15e41528c3
samsab1995 <samirsabiee@gmail.com>
2021-09-07 15:00:07 +0430

    set relation change routeKeyName to uuid and auto generate uuid when model created

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id', 'uuid'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return 'uuid';
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (Model $model) {
+            $model->attributes['uuid'] = Str::uuid()->toString();
+        });
+    }
 }
