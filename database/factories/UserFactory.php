eaca1669f0ca2dd215944a7ab5499f9e035e264d
samsab1995 <samirsabiee@gmail.com>
2021-09-07 15:05:47 +0430

    add extra field for factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,11 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'phone' => $this->faker->numerify('09#########'),
+            'country' => $this->faker->country(),
+            'city' => $this->faker->city(),
             'remember_token' => Str::random(10),
+            'created_at' => $this->faker->dateTimeBetween('-2 years'),
         ];
     }
 
