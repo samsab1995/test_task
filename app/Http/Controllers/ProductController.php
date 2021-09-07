a68c92a83913ffde8e20c01283bbfd8e202ed618
samsab1995 <samirsabiee@gmail.com>
2021-09-07 15:00:41 +0430

    complete crud operations

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,62 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use App\Repository\Eloquent\ProductRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ProductController extends Controller
 {
-    //
+    private ProductRepository $productRepository;
+
+    /**
+     * ProductController constructor.
+     */
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->middleware('auth:api');
+        $this->productRepository = $productRepository;
+        $this->authorizeResource(Product::class, 'product');
+    }
+
+    public function index(): AnonymousResourceCollection
+    {
+        $products = $this->productRepository->paginate();
+        return ProductResource::collection($products);
+    }
+
+    public function store(StoreProductRequest $request): ProductResource
+    {
+        $product = $this->productRepository->create($request->validated());
+        return new ProductResource($product);
+
+    }
+
+    public function show(Product $product): ProductResource
+    {
+        return new ProductResource($product);
+    }
+
+    public function update(Product $product, UpdateProductRequest $request): JsonResponse
+    {
+        $status = $this->productRepository->update($product->uuid, $request->validated());
+        return $this->updateOrDeleteResponse($status);
+    }
+
+    public function destroy(Product $product): JsonResponse
+    {
+        $status = $this->productRepository->delete($product->uuid);
+        return $this->updateOrDeleteResponse($status);
+    }
+
+    private function updateOrDeleteResponse($status): JsonResponse
+    {
+        return response()->json([
+            'status' => $status
+        ]);
+    }
 }
