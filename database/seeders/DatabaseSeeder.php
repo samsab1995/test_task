874d507be82d5792e1455eebd09e16252dd767c3
samsab1995 <samirsabiee@gmail.com>
2021-09-07 14:57:10 +0430

    seed one admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -17,5 +18,6 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(10)->create();
         Product::factory(20)->create();
+        Admin::factory(1)->create();
     }
 }
