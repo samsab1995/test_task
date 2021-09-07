b285d462b65eab82ced2bd130a0377bf84a02441
samsab1995 <samirsabiee@gmail.com>
2021-09-07 14:53:33 +0430

    create one admin user

---
+++ database/factories/AdminFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Admin;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class AdminFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Admin::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => 'Samir',
+            'email' => 'sabieesamir@gmail.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'phone' => '09362633788',
+            'country' => 'Iran',
+            'city' => 'Boushehr',
+        ];
+    }
+}
