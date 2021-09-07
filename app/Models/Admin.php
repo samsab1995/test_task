387a1b379389641bec8e98fa2ced52cf2e51d3dd
samsab1995 <samirsabiee@gmail.com>
2021-09-07 14:52:24 +0430

    create admin model and make it Authenticatable

---
+++ app/Models/Admin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Str;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Admin extends Authenticatable implements JWTSubject
+{
+    use HasFactory;
+
+    protected $guarded = ['id', 'uuid'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (Model $model) {
+            $model->attributes['uuid'] = Str::uuid()->toString();
+        });
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(): array
+    {
+        return [];
+    }
+}
